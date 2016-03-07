eb36e7e337c21241bada4d32cb50833ba028af26
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-03-08 12:40:51 +1300

    add asteroid spawn function

--- racer.js
+++ racer.js
@@ -2,47 +2,140 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     var distance = 10; //how long the race track is
-    var ship1Pos = 0; //ship1's position
-    var ship2Pos = 0; //ship2's position
-    var ship1Positions = document.querySelectorAll("tr.player1_strip td"); //a list of all the table cells in player1's track
-    var ship2Positions = document.querySelectorAll("tr.player2_strip td"); //a list of all the table cells in player2's track
+    var ships = [];
+    var asteroids = [];
+    var rows = [
+      {
+        element: document.querySelector("tr.strip.a"),
+        name: "generic",
+        row: 0,
+        querySelector: "tr.strip.a td",
+        rowCells: document.querySelectorAll("tr.strip.a td"),
+        objectsPos: []
+      },
+      {
+        element: document.querySelector("tr.strip.b"),
+        name: "generic",
+        row: 1,
+        querySelector: "tr.strip.b td",
+        rowCells: document.querySelectorAll("tr.strip.b td"),
+        objectsPos: []
+      },
+      { 
+        element: document.querySelector("tr.player1_strip"), //player1's track
+        name: "ship-1",
+        row: 2, 
+        querySelector: "tr.player1_strip td",
+        rowCells: document.querySelectorAll("tr.player1_strip td"), //a list of all the cells in player1's track
+        objectsPos: [
+          {
+            name: "ship-1",
+            position: 0
+          }
+        ]
+      }, 
+      {
+        element: document.querySelector("tr.player2_strip"), //player2's track
+        name: "ship-2",
+        row: 3,
+        querySelector: "tr.player2_strip td",
+        rowCells: document.querySelectorAll("tr.player2_strip td"), //a list of all the cells in player2's track
+        objectsPos: [
+          {
+            name: "ship-2",
+            position: 0
+          }
+        ]
+      },
+      {
+        element: document.querySelector("tr.strip.c"),
+        name: "generic",
+        row: 4,
+        querySelector: "tr.strip.c td",
+        rowCells: document.querySelectorAll("tr.strip.c td"),
+        objectsPos: []
+      },
+      {
+        element: document.querySelector("tr.strip.d"),
+        name: "generic",
+        row: 5,
+        querySelector: "tr.strip.d td",
+        rowCells: document.querySelectorAll("tr.strip.d td"),
+        objectsPos: []
+      }, 
+    ];
+
+    function Space_Object( name, x, y, dir) {
+      this.name = name;
+      this.posX = x;
+      this.posY = y;
+      this.cell = rows[this.posY].rowCells[this.posX];
+      this.direction = dir; 
+      this.move = function() { 
+        this.cell.className = this.cell.className.replace(this.name, '');
+        if(this.direction === "up") {
+          this.posY--;
+          if(this.posY < 0)
+            this.posY = 5;
+        }
+        else if(this.direction === "down") {
+          this.posY++;  
+          if(this.posY > 5)
+            this.posY = 0;      
+        }
+        else if(this.direction === "right") {
+          this.posX++;                 
+        }
+        this.cell = rows[this.posY].rowCells[this.posX];
+        this.cell.className = this.name;
+      };     
+      this.spawn = function() {
+        this.cell.className = this.name;
+      }
+    }
+
+    function Ship(name, x, y, dir) {
+      Space_Object.call(this, name, x, y, dir);
+    }
+
+    function Asteroid(name, x, y, dir) {
+      Space_Object.call(this, name, x, y, dir);
+    }
 
     newGame(); //start a new game
     
-    function updatePlayerPosition(ship) {  
-      //if ship1 is moving, iterate through the list of table cells to find ship1, remove the 'ship-1' class, and add that class to the next table cell along      
-      if(ship === "ship-1") {
-        for (var i = 0; i < ship1Positions.length; ++i) {        
-          if(i === ship1Pos) {
-            ship1Positions[ship1Pos].className = ship1Positions[ship1Pos].className.replace( /(?:^|\s)ship-1(?!\S)/g , '' );
-            ship1Pos += 1;            
-            ship1Positions[ship1Pos].className = "ship-1";
-            break;
-          }
-        }  
-        //if ship1's new position is = to the distance of the race track, signal victory to player1             
-        if(ship1Pos == distance) {
-          alert("Player 1 wins!")
-          document.removeEventListener("keyup", checkKeyPressed, false); 
-        }   
+    function updatePlayerPosition(ship) {
+      var track;
+      var shipPos;
+      var index;
+      
+      //check which ship is being called on and set variables accordingly
+      for(var i=2; i<=3; i++) {
+        if(rows[i].name === ship) {
+          track = rows[i].rowCells;
+          shipPos = rows[i].objectsPos[0].position;
+          index = i;
+        }
       }
-      //if ship2 is moving, iterate through the list of table cells to find ship2, remove the 'ship-2' class, and add that class to the next table cell along             
-      else if(ship === "ship-2") {        
-        for (var i = 0; i < ship2Positions.length; ++i) {        
-          if(i === ship2Pos) {
-            ship2Positions[ship2Pos].className = ship2Positions[ship2Pos].className.replace( /(?:^|\s)ship-2(?!\S)/g , '' );
-            ship2Pos += 1;
-            ship2Positions[ship2Pos].className = "ship-2";
-            break;
-          }
-        }   
-        //if ship2's new position is = to the distance of the race track, signal victory to player2              
-        if(ship2Pos == distance) {
-          alert("Player 2 wins!")
-          document.removeEventListener("keyup", checkKeyPressed, false); 
-        }    
-      }        
-    }
+      //find ship on track and move it
+      for(var i=0; i<track.length; i++) {
+        if(i === shipPos) {
+          track[shipPos].className = track[shipPos].className.replace(ship , '' );
+          shipPos ++;
+          track[shipPos].className = ship;
+          rows[index].objectsPos[0].position = shipPos;
+          break;
+        }
+      }
+      //check for asteroid collision
+      if(shipPos)
+      //check for victory
+      if(shipPos == distance) {
+        alert(ship + " wins!");
+        document.removeEventListener("keyup", checkKeyPressed, false); 
+        stopAsteroids();
+      }
+    }     
 
     //check which key activated the eventlistener . If 81(q) move ship1 forward. Else if 80(p) move ship2 forward
     function checkKeyPressed(e) {        
@@ -56,38 +149,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //start a new game
     function newGame() {      
-      returnShipToStart("ship-1"); //return ship1 to start
-      returnShipToStart("ship-2"); //return ship2 to start
+      populateGalaxy();
+      returnShipsToStart();
       document.addEventListener("keyup", checkKeyPressed, false); //turn on eventlistener for keyboard
       setDistance(distance); //reset the distance variable 
-      ship1Positions = document.querySelectorAll("tr.player1_strip td"); //reset the lists of table cells
-      ship2Positions = document.querySelectorAll("tr.player2_strip td");    
+      spawnAsteroids();      
     }
 
-    //return ships to starting positions. First find each ship's position on the table, then remove the "ship" class from that cell and append it to the starting cell. Reset the variable for the position of each ship.
-    function returnShipToStart(ship) {
-      if(ship === "ship-1") {
-        for (var i = 0; i < ship1Positions.length; ++i) {        
-          if(i === ship1Pos) {
-            ship1Positions[ship1Pos].className = ship1Positions[ship1Pos].className.replace( /(?:^|\s)ship-1(?!\S)/g , '' );
-            ship1Pos = 0;
-            ship1Positions[ship1Pos].className = "ship-1";
-            break;
-          }
-        } 
-      }    
-      else if(ship === "ship-2") {
-        for (var i = 0; i < ship2Positions.length; ++i) {        
-          if(i === ship2Pos) {
-            ship2Positions[ship2Pos].className = ship2Positions[ship2Pos].className.replace( /(?:^|\s)ship-2(?!\S)/g , '' );
-            ship2Pos = 0;
-            ship2Positions[ship2Pos].className = "ship-2";
-            break;
-          }
-        }
-      }    
-    }
-
+    /*function returnShipsToStart() {      
+      for(var i=2; i<=3; i++) {
+        rows[i].rowCells[0].className = "ship-" + (i-1);
+        rows[i].objectsPos[0].position = 0;
+      }
+    }*/
+      
     //fetch the value of the selected option in the distance select panel, then reset the game
     function changeDistance(e) {      
       distance = e.srcElement.value || e.target.value;
@@ -96,52 +171,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //create the track according to selected distance, but first delete the current track
     function setDistance(dist) {            
-      var rows = [
-        {
-          elements: document.querySelectorAll("tr.strip"),
-          name: "gen",
-          rowsCells: 
-            [
-              document.querySelectorAll("tr.strip.a td"),
-              document.querySelectorAll("tr.strip.b td"),
-              document.querySelectorAll("tr.strip.c td"),
-              document.querySelectorAll("tr.strip.d td")
-            ]
-        },
-        { 
-          elements: document.querySelectorAll("tr.player1_strip"), //player1's track
-          name: "p1row", 
-          rowsCells: [document.querySelectorAll("tr.player1_strip td")] //a list of all the cells in player1's track
-        }, 
-        {
-          elements: document.querySelectorAll("tr.player2_strip"), //player2's track
-          name: "p2row",
-          rowsCells: [document.querySelectorAll("tr.player2_strip td")] //a list of all the cells in player2's track
-        }  
-      ];
-
       rows = rows.map(function(row) {
-        var rowLength = row.rowsCells[0].length;
-        console.log(row.rowsCells[0].length);
-        console.log(row.rowsCells[0][0]);
+        var rowLength = row.rowCells.length;
         for(var i=rowLength; i>1; i--) {
-          for(var j=0; j<row.elements.length; j++) {
-            console.log(row.rowsCells[j][i-1]);
-            row.elements[j].removeChild(row.rowsCells[j][i-1]);
-          }
+          row.element.removeChild(row.rowCells[i-1]);   
         }
         for(var i=0; i<dist; i++) {
-          for(var j=0; j<row.elements.length; j++) {    
-            var newCell = document.createElement("td"); //new cell
-            row.elements[j].appendChild(newCell); //add new cells to end of row   
-          }                 
+          var newCell = document.createElement("td"); //new cell
+          row.element.appendChild(newCell); //add new cells to end of row 
+          row.rowCells = document.querySelectorAll(row.querySelector);          
         } 
-        if(row.name === "p1row") {
-          row.elements[0].innerHTML = row.elements[0].innerHTML + "<td rowspan = \"2\" class = \"finish_line\" style = \"width:300;\"></td>";
+        if(row.name === "ship-1") {
+          row.element.innerHTML = row.element.innerHTML + "<td rowspan = \"2\" class = \"finish_line\"></td>";
+          row.rowCells = document.querySelectorAll(row.querySelector); 
         }       
         return row;        
       });
-    }    
+    }  
+
+    var intervalID;
+    function populateGalaxy() {
+      ships.push(new Ship("ship-1", 0, 2, "right"));
+      ships.push(new Ship("ship-2", 0, 3, "right"));
+      for(var i=0; i<ships.length; i++)
+        ships[i].spawn();
+      asteroids.push(new Asteroid("asteroid", 5, 5, "up"));
+      for(var i=0; i<asteroids.length; i++) {
+        asteroids[i].spawn();
+        intervalID = setInterval(function() { 
+          asteroids[i].move();
+        }, 500);
+      }
+    } 
+
+    function stopAsteroids() {
+      clearInterval(intervalID);
+    } 
     
     //set up eventListener for the 'New Game' button    
     var newGameBtn = document.getElementById("newGame");
